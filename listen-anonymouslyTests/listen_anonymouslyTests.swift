660a12ef9f3dfdbed249ca71aa507514d985a252
lfcj <fernanda16400@gmail.com>
2025-10-12 20:50:08 +0200

    Add empty project

---
+++ listen-anonymouslyTests/listen_anonymouslyTests.swift
@@ -0,0 +1,17 @@
+//
+//  listen_anonymouslyTests.swift
+//  listen-anonymouslyTests
+//
+//  Created by Luisa Castaño on 12.10.25.
+//
+
+import Testing
+@testable import listen_anonymously
+
+struct listen_anonymouslyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

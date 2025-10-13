c45417e6478930a12ba076ff1d67a545fee8df02
lfcj <fernanda16400@gmail.com>
2025-10-13 11:25:25 +0200

    Test isLoadingAudio and errorMessage when extensionContext is empty

--- Listen Anonymously SharedTests/AudioPlayingManagerTests.swift
+++ Listen Anonymously SharedTests/AudioPlayingManagerTests.swift
@@ -5,13 +5,23 @@ struct AudioPlayingManagerTests {
 
     @Test("Calling findAudio marks isLoadingAudio to true")
     func findAudio_marksIsLoadingAudioProperty() async throws {
-        let manager = AudioPlayingManager(extensionContext: nil)
+        let manager = AudioPlayingManager(extensionContext: FakeExtensionContext())
 
         #expect(manager.isLoadingAudio == false, "Initial value should be false")
 
         await manager.findAudio()
 
         #expect(manager.isLoadingAudio == true, "isLoadingAudio should be true when it is finding audio")
+    }
+
+    @Test("Empty extensionContext stops audio search and shows error message")
+    func emptyExtensionContext_stopsAudioSearchAndShowsErrorMessage() async throws {
+        let manager = AudioPlayingManager(extensionContext: nil)
+        
+        await manager.findAudio()
+
+        #expect(manager.errorMessage == "No audio file could be find. Please check you selected only one file.")
+
     }
 
 }
---
+++ Listen Anonymously SharedTests/Fake/FakeExtensionContext.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+final class FakeExtensionContext: NSExtensionContext {
+    override var inputItems: [Any] {
+        []
+    }
+}

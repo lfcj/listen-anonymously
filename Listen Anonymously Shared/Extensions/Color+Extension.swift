770d2c01e1d10e99a91a750f09128eff1735f7cc
lfcj <fernanda16400@gmail.com>
2025-10-17 20:52:04 +0200

    Add trailing space

--- Listen Anonymously Shared/Extensions/Color+Extension.swift
+++ Listen Anonymously Shared/Extensions/Color+Extension.swift
@@ -3,5 +3,5 @@ import SwiftUI
 extension Color {
     static var la_purple: Color { Color(uiColor: #colorLiteral(red: 0.2862745098, green: 0.003921568627, blue: 0.5098039216, alpha: 1)) }
     static var la_magenta: Color { Color(uiColor: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)) }
-    static var la_teal: Color { Color(uiColor: #colorLiteral(red: 0, green: 0.6117647059, blue: 0.7254901961, alpha: 1))}
+    static var la_teal: Color { Color(uiColor: #colorLiteral(red: 0, green: 0.6117647059, blue: 0.7254901961, alpha: 1)) }
 }

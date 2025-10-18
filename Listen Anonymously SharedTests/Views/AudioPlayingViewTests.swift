72f1025c02627673bf4684d9dee61b0e63a8fdef
lfcj <fernanda16400@gmail.com>
2025-10-18 10:51:57 +0200

    Make sure error message is shown when it exists and disappears when it is set to nil

--- Listen Anonymously SharedTests/Views/AudioPlayingViewTests.swift
+++ Listen Anonymously SharedTests/Views/AudioPlayingViewTests.swift
@@ -32,4 +32,22 @@ struct AudioPlayingViewTests {
         #expect(try inspectedView.vStack().padding() == .init(top: 10, leading: 10, bottom: 10, trailing: 10))
     }
 
+    @Test func displaysErrorMessageWhenThereIsOne() throws {
+        let manager = AudioPlayingManager(extensionContext: nil)
+        manager.errorMessage = "Expected error message"
+        let view = AudioPlayingView(playingManager: manager)
+        let errorMessageView = try view.inspect().zStack(0).view(ErrorMessageView.self, 1)
+        #expect(errorMessageView.isHidden() == false)
+
+        manager.errorMessage = nil
+
+        do {
+            let _ = try view.inspect().zStack(0).view(ErrorMessageView.self, 1)
+            #expect(manager.errorMessage == "this should never be called")
+        } catch let error {
+            // "View for Optional<_ConditionalContent<AudioLoadingView, ErrorMessageView>> is absent"
+            #expect(error.localizedDescription.contains("is absent"))
+        }
+    }
+
 }

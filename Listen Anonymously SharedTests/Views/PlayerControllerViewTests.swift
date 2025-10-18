cc65e7430c8b6b41ebd3fbfdf7632e0f198ef553
lfcj <fernanda16400@gmail.com>
2025-10-18 11:27:50 +0200

    Test that initial icon is the play one

---
+++ Listen Anonymously SharedTests/Views/PlayerControllerViewTests.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+import Testing
+import ViewInspector
+@testable import Listen_Anonymously_Shared
+
+@MainActor
+struct PlayerControllerViewTests {
+
+    @Test func initialButtonIsPlayIcon() throws {
+        let manager = AudioPlayingManager(extensionContext: nil)
+
+        let view = PlayerControllerView(playingManager: manager)
+        let inspectedButtonImageName = try view.inspect().vStack()
+            .hStack(2)
+            .view(PlayingViewButton.self, 1)
+            .button(0)
+            .labelView()
+            .image(0)
+            .actualImage()
+            .name()
+
+        #expect(inspectedButtonImageName == "play.fill")
+    }
+ 
+}
+

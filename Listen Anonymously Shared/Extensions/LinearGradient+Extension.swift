ab9d0cbf8f6ee0b039886c92ebad90000cc44ca2
lfcj <fernanda16400@gmail.com>
2025-10-19 15:32:51 +0200

    Add linear gradient to be reused from Shared framework

---
+++ Listen Anonymously Shared/Extensions/LinearGradient+Extension.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+public extension LinearGradient {
+
+    static var lavenderToPastelBlue: LinearGradient {
+
+        
+        LinearGradient(
+            colors: [
+                .lavender,
+                .violetLavender,
+                .pastelBlue
+            ],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+    }
+
+}
+
+#Preview {
+    ZStack {
+        LinearGradient.lavenderToPastelBlue
+            .ignoresSafeArea()
+        Text("Hi")
+            .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    ZStack {
+        LinearGradient.lavenderToPastelBlue
+            .ignoresSafeArea()
+        Text("Hi")
+            .foregroundStyle(.white)
+    }
+    .preferredColorScheme(.dark)
+}

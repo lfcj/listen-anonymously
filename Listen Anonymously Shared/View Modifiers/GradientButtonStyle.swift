76f300f818028930b0d89f94fb8b3dd4bf0a36cb
lfcj <fernanda16400@gmail.com>
2025-10-19 16:40:37 +0200

    Add GradientButtonStyle

---
+++ Listen Anonymously Shared/View Modifiers/GradientButtonStyle.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+public struct GradientButtonStyle: ButtonStyle {
+
+    public init() {}
+
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.headline)
+            .padding(.vertical, 14)
+            .frame(maxWidth: .infinity)
+            .background(
+                LinearGradient(
+                    gradient: Gradient(colors: [.violetLavender, .pastelBlue]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+            )
+            .cornerRadius(24)
+            .scaleEffect(configuration.isPressed ? 0.98 : 1.0)
+            .shadow(
+                color: .pastelBlue.opacity(configuration.isPressed ? 0.18 : 0.28),
+                radius: configuration.isPressed ? 6 : 18, x: 0, y: 8
+            )
+            .foregroundColor(.white)
+            .padding(.horizontal, 8)
+    }
+
+}
+
+struct GradientButtonStyleLight_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Button("Hello, World!") {}
+                .buttonStyle(GradientButtonStyle())
+        }.preferredColorScheme(.light)
+    }
+}
+
+struct GradientButtonStyleDark_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Button("Hello, World!") {}
+                .buttonStyle(GradientButtonStyle())
+        }
+        .preferredColorScheme(.dark)
+    }
+}

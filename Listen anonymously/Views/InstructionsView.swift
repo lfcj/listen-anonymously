78c152018b11d21d5b8a510878044e00b8cc13e6
lfcj <fernanda16400@gmail.com>
2025-10-21 13:16:12 +0200

    Add Instructions view making sure we show instructions for Whatsapp or Telegram

--- Listen anonymously/Views/InstructionsView.swift
+++ Listen anonymously/Views/InstructionsView.swift
@@ -1,7 +1,70 @@
+import Listen_Anonymously_Shared
 import SwiftUI
+import UIKit
+
+class InstructionsViewModel: ObservableObject {
+    @Published var supportedApps: [SupportedApps] = []
+    @Published var selectedApp: SupportedApps?
+    
+    var needsPicker: Bool {
+        supportedApps.count > 1
+    }
+    
+    init() {
+        self.supportedApps = SupportedApps.allCases.filter({ DeeplinkHelper.hasApp($0) })
+        self.selectedApp = supportedApps.first
+    }
+}
 
 struct InstructionsView: View {
+    @StateObject var viewModel = InstructionsViewModel()
+
     var body: some View {
-        Text("Show instructions here!")
+        ZStack {
+            LinearGradient.lavenderToPastelBlue.ignoresSafeArea()
+
+            VStack {
+                HStack {
+                    Label("How to use?", systemImage: "list.number")
+                        .font(.system(size: 44, weight: .bold, design: .rounded))
+                        .foregroundStyle(.white)
+                        .bold()
+                }
+                .padding([.top, .bottom])
+
+                if viewModel.needsPicker {
+                    VStack {
+                        Picker("Supported App", selection: $viewModel.selectedApp) {
+                            ForEach(viewModel.supportedApps) { app in
+                                Text(app.rawValue).tag(app)
+                            }
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                switch viewModel.selectedApp {
+                case .none, .whatsApp:
+                    WhatsAppInstructionsStepsView()
+                        .padding()
+                case .telegram:
+                    TelegramInstructionsStepsView()
+                        .padding()
+                @unknown default:
+                    fatalError("Support \(String(describing: viewModel.selectedApp))")
+                }
+            }
+            .padding()
+        }
     }
+
+}
+
+#Preview {
+    InstructionsView()
+}
+
+#Preview {
+    InstructionsView()
+        .preferredColorScheme(.dark)
 }

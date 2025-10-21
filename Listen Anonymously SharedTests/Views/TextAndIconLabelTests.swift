80ae7ab06f1d87c0e335f4db778ad140d1d36292
lfcj <fernanda16400@gmail.com>
2025-10-21 15:04:28 +0200

    Add tests for `TextAndIconLabel`

--- Listen Anonymously SharedTests/Views/TextAndIconLabelTests.swift
+++ Listen Anonymously SharedTests/Views/TextAndIconLabelTests.swift
@@ -1,3 +1,27 @@
-//
+import SwiftUI
+import Testing
+import ViewInspector
+@testable import Listen_Anonymously_Shared
 
-import Foundation
+@MainActor
+struct TextAndIconLabelTests {
+
+    @Test func systemImageIsDisplayed_whenAvailable() throws {
+        let textAndIconLabel = TextAndIconLabel(title: "bla", systemNameOrEmoji: "xmark")
+
+        let inspectedImageName = try textAndIconLabel.inspect().hStack(0).view(SafeImage.self, 1).image(0).actualImage().name()
+
+        #expect(inspectedImageName == "xmark")
+    }
+
+    @Test func noIconIsDisplayed_whenImageNameIsEmpty() throws {
+        let textAndIconLabel = TextAndIconLabel(title: "bla", systemNameOrEmoji: "")
+
+        do {
+            _ = try textAndIconLabel.inspect().hStack(0).view(SafeImage.self, 1)
+        } catch let error {
+            #expect(error.localizedDescription.contains("is absent"))
+        }
+    }
+
+}

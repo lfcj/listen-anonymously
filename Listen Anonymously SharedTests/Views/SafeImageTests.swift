7ec14dbee40e10199d260932342f031aedf963c5
lfcj <fernanda16400@gmail.com>
2026-02-12 11:01:19 +0000

    Test SafeImage without ViewInspector

--- Listen Anonymously SharedTests/Views/SafeImageTests.swift
+++ Listen Anonymously SharedTests/Views/SafeImageTests.swift
@@ -1,16 +1,15 @@
+import Listen_Anonymously_Shared
 import Testing
-@testable import Listen_Anonymously_Shared
 
-@MainActor
 struct SafeImageTests {
 
     @Test func safeImageDisplaysText_whenSystemImageDoesNotExist() throws {
-        let content = SafeImageContent.resolve(name: "does-not-exist")
+        let content = SafeImageContent.resolve(name: "does-not-exist", systemImageExists: { _ in false })
         #expect(content == .text("does-not-exist"))
     }
 
     @Test func safeImageDisplaysSystemImage_whenNameMatchesSystemCatalog() throws {
-        let content = SafeImageContent.resolve(name: "xmark")
+        let content = SafeImageContent.resolve(name: "xmark", systemImageExists: { _ in true })
         #expect(content == .systemImage("xmark"))
     }
 

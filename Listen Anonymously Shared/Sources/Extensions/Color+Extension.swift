0c59150e7e4e947972572d6c5aee8bff06b66a9d
lfcj <fernanda16400@gmail.com>
2026-02-26 21:51:41 +0100

    Add access to xcassets colors via extension as Tuist does not provide access to colors set in xcassets

--- Listen Anonymously Shared/Sources/Extensions/Color+Extension.swift
+++ Listen Anonymously Shared/Sources/Extensions/Color+Extension.swift
@@ -15,4 +15,21 @@ public extension Color {
         )
     }
 
+    // MARK: - Asset catalog colors (Media.xcassets/Colors)
+
+    static let laTeal = Color("la_teal", bundle: .module)
+    static let laPurple = Color("la_purple", bundle: .module)
+    static let laMagenta = Color("la_magenta", bundle: .module)
+    static let laPurpleText = Color("la_purpleText", bundle: .module)
+    static let lavender = Color("lavender", bundle: .module)
+    static let pastelBlue = Color("pastel-blue", bundle: .module)
+    static let violetLavender = Color("violet-lavender", bundle: .module)
+    static let buttonStart = Color("buttonStart", bundle: .module)
+    static let buttonEnd = Color("buttonEnd", bundle: .module)
+    static let buttonShadow = Color("buttonShadow", bundle: .module)
+    static let iconButtonBackgroundStart = Color("iconButtonBackgroundStart", bundle: .module)
+    static let iconButtonBackgroundEnd = Color("iconButtonBackgroundEnd", bundle: .module)
+    static let iconButtonForegroundStart = Color("iconButtonForegroundStart", bundle: .module)
+    static let iconButtonForegroundEnd = Color("iconButtonForegroundEnd", bundle: .module)
+
 }

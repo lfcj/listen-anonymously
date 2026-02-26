1878023e06aebae359c7c8075271b85ed8816644
lfcj <fernanda16400@gmail.com>
2026-02-26 21:48:28 +0100

    Use exact SPM packages and not frameworks to avoid Tuist handing while downloading RevenueCat

--- Package.swift
+++ Package.swift
@@ -1,23 +1,11 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
-#if TUIST
-    import ProjectDescription
-
-    let packageSettings = PackageSettings(
-        productTypes: [
-            "RevenueCat": .framework,
-            "PostHog": .framework,
-            "ViewInspector": .framework
-        ]
-    )
-#endif
-
 let package = Package(
     name: "ListenAnonymously",
     dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.0.0"),
-        .package(url: "https://github.com/PostHog/posthog-ios.git", from: "3.0.0"),
-        .package(url: "https://github.com/nalexn/ViewInspector.git", from: "0.9.0")
+        .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "5.59.2"),
+        .package(url: "https://github.com/PostHog/posthog-ios.git", from: "3.41.1"),
+        .package(url: "https://github.com/nalexn/ViewInspector.git", from: "0.10.3")
     ]
 )

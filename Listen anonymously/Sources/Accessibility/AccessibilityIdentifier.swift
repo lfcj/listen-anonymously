d45067c5a863c0d53fbcc43a00a6c4d464852e4e
lfcj <fernanda16400@gmail.com>
2026-02-28 20:02:51 +0100

    Localize in all other languages

---
+++ Listen anonymously/Sources/Accessibility/AccessibilityIdentifier.swift
@@ -0,0 +1,19 @@
+public enum AccessibilityIdentifier {
+    public enum Donations {
+        public static let buyUsCoffeeButton = "buyUsCoffeeButton"
+        public static let sendGoodVibesButton = "sendGoodVibesButton"
+        public static let sendSuperKindTip = "sendSuperKindTip"
+    }
+    public enum FrontDoor {
+        public static let seeInstructions = "seeInstructions"
+    }
+    public enum Instructions {
+        public static let picker = "instructionsPicker"
+        public static let whatsAppStep4 = "instructions_whatsapp_step4"
+        public static let telegramStep2 = "instructions_telegram_step2"
+    }
+    public enum Titles {
+        public static let stayUnseen = "stayUnseen"
+        public static let stillInTheLoop = "stillInTheLoop"
+    }
+}

6db9b7790732d773a907e68cd015553ef00eebba
lfcj <fernanda16400@gmail.com>
2026-02-28 21:36:36 +0100

    Make RevenueCat mock entities available for all tests

--- Listen anonymouslyTests/Sources/RevenueCatServiceTests.swift
+++ Listen anonymouslyTests/Sources/RevenueCatServiceTests.swift
@@ -5,30 +5,6 @@ import XCTest
 
 final class RevenueCatServiceTests: XCTestCase {
 
-    private final class MockPurchasesClient: PurchasesClient, @unchecked Sendable {
-        var isConfigured: Bool = false
-        var configuredWithKey: String?
-
-        var productsByID: [String: StoreProductProtocol] = [:]
-        var purchaseResult: ProductPurchaseResult?
-        var purchaseError: Error?
-
-        func configure(withAPIKey key: String) {
-            isConfigured = true
-            configuredWithKey = key
-        }
-
-        func getProducts(_ productIDs: [String], completion: @escaping ([StoreProductProtocol]) -> Void) {
-            let products = productIDs.compactMap { productsByID[$0] }
-            completion(products)
-        }
-
-        func purchase(product: StoreProductProtocol) async throws -> ProductPurchaseResult {
-            if let error = purchaseError { throw error }
-            return purchaseResult ?? ProductPurchaseResult(transaction: nil, customerInfo: DummyCustomerInfo(), userCancelled: false)
-        }
-    }
-
     private let postHogSpy = PostHogSpy()
 
     func test_configureRevenueCat_missingAPIKey_logsFailure() async throws {
@@ -186,14 +162,38 @@ final class PostHogSpy: PostHogProtocol, @unchecked Sendable {
     }
 }
 
-private struct DummyCustomerInfo: CustomerInfoProtocol {}
-private struct DummyStoreProduct: StoreProductProtocol {
+struct DummyCustomerInfo: CustomerInfoProtocol {}
+struct DummyStoreProduct: StoreProductProtocol {
     var productIdentifier: String
 }
-private struct DummyStoreTransaction: StoreTransactionProtocol {}
+struct DummyStoreTransaction: StoreTransactionProtocol {}
 
-private struct MockRevenueCatConfig: RevenueCatConfigProviding {
+struct MockRevenueCatConfig: RevenueCatConfigProviding {
     let apiKey: String
 
     static let empty: MockRevenueCatConfig = MockRevenueCatConfig(apiKey: "")
+}
+
+final class MockPurchasesClient: PurchasesClient, @unchecked Sendable {
+    var isConfigured: Bool = false
+    var configuredWithKey: String?
+
+    var productsByID: [String: StoreProductProtocol] = [:]
+    var purchaseResult: ProductPurchaseResult?
+    var purchaseError: Error?
+
+    func configure(withAPIKey key: String) {
+        isConfigured = true
+        configuredWithKey = key
+    }
+
+    func getProducts(_ productIDs: [String], completion: @escaping ([StoreProductProtocol]) -> Void) {
+        let products = productIDs.compactMap { productsByID[$0] }
+        completion(products)
+    }
+
+    func purchase(product: StoreProductProtocol) async throws -> ProductPurchaseResult {
+        if let error = purchaseError { throw error }
+        return purchaseResult ?? ProductPurchaseResult(transaction: nil, customerInfo: DummyCustomerInfo(), userCancelled: false)
+    }
 }
